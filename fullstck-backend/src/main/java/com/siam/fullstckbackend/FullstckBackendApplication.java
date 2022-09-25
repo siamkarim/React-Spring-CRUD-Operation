ba9b6db4c09d0102e2a6a814c296b9842215a4f8
siamkarim <siamhossain518@gmail.com>
2022-09-25 16:32:53 +0600

    model create

---
+++ fullstck-backend/src/main/java/com/siam/fullstckbackend/FullstckBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siam.fullstckbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstckBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstckBackendApplication.class, args);
+	}
+
+}
