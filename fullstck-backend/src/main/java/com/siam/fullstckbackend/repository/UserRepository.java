5e57cec03ac6ac2aa1403a1c6196e1b7233a299f
siamkarim <siamhossain518@gmail.com>
2022-09-25 16:53:25 +0600

    creating user repository

--- fullstck-backend/src/main/java/com/siam/fullstckbackend/repository/UserRepository.java
+++ fullstck-backend/src/main/java/com/siam/fullstckbackend/repository/UserRepository.java
@@ -1,1 +1,6 @@
+package com.siam.fullstckbackend.repository;
+import com.siam.fullstckbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface UserRepository  extends JpaRepository<User,Long>{
+}
